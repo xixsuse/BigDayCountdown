902b6d449ee31881467548aa434c4609817e5dac
smholsen <smholsen@stud.ntnu.no>
2015-10-23 18:06:08 +0200

    Added backgrounds for different screen sizes -> Does not show up when run...

--- app/src/main/java/com/simonm/bigdaycountdown/MainActivity.java
+++ app/src/main/java/com/simonm/bigdaycountdown/MainActivity.java
@@ -1,9 +1,5 @@
 package com.simonm.bigdaycountdown;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.SearchManager;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,36 +8,39 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Variables
+    protected int NumberOfDatesTracked;
 
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
 
     protected CharSequence mTitle;
-    private String[] mPlanetTitles;
+    private String[] menuTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        RelativeLayout main_view = (RelativeLayout) findViewById(R.id.content_main_id);
+        RelativeLayout get_started_view = (RelativeLayout) findViewById(R.id.content_get_started_id);
+
+        startScreenCheck(getNumberOfDatesTracked(), get_started_view, main_view);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        menuTitles = getResources().getStringArray(R.array.menu_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
@@ -60,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         // set up the drawer's list view with items and click listener
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
+                R.layout.drawer_list_item, menuTitles));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
 
@@ -87,13 +86,11 @@ public class MainActivity extends AppCompatActivity {
             selectItem(0);
         }
     }
-
     // --------- END OF OnCreate()
 
-    // Variables
-    protected int NumberOfDatesTracked;
 
     public int getNumberOfDatesTracked() {
+
         return NumberOfDatesTracked;
     }
 
@@ -102,17 +99,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Check to see if any dates are tracked, if not -> We display a get Started Screen.
-    public void startScreenCheck(){
+    public void startScreenCheck(int numberOfDatesTracked, View main_view, View get_started_view){
         // Initialize the different views
-        RelativeLayout content_main_view = (RelativeLayout) findViewById(R.id.content_main_id);
-        RelativeLayout content_getStarted_view = (RelativeLayout) findViewById(R.id.content_getStarted_id);
-
-        if (getNumberOfDatesTracked() == 0){
-            content_getStarted_view.setVisibility(View.VISIBLE);
-            content_main_view.setVisibility(View.GONE);
+        Log.i("tag", String.valueOf(numberOfDatesTracked));
+        if (numberOfDatesTracked == 0){
+            get_started_view.setVisibility(View.VISIBLE);
+            main_view.setVisibility(View.GONE);
         } else {
-            content_main_view.setVisibility(View.VISIBLE);
-            content_getStarted_view.setVisibility(View.GONE);
+            main_view.setVisibility(View.VISIBLE);
+            get_started_view.setVisibility(View.GONE);
         }
 
     }
@@ -129,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
         // If the nav drawer is open, hide action items related to the content view
         boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -142,20 +136,8 @@ public class MainActivity extends AppCompatActivity {
         }
         // Handle action buttons
         switch(item.getItemId()) {
-            case R.id.action_websearch:
-                // create intent to perform web search for this planet
-                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-                // catch event that there's no activity to handle intent
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
         }
+        return super.onOptionsItemSelected(item);
     }
 
     /* The click listner for ListView in the navigation drawer */
@@ -170,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
 
         // update selected item and title, then close the drawer
         mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
+        setTitle(menuTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
